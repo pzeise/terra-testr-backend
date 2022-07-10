2f0be8e9798758311596f4217e1ad1901bf2f3ee
pzeise <phillip.zeise@gmail.com>
2022-07-10 18:42:18 -0400

    setup up intial routes

---
+++ models/answer-model.js
@@ -0,0 +1,25 @@
+const mongoose = require('../db/connection')
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+const AnswerSchema = new mongoose.Schema(
+    {
+        id: {
+            type: String, 
+            required: true
+        },
+        locations: 
+            [{
+                lat: {type: Number},
+                lng: {type: Number}
+            }],
+        endState: {
+            title: String,
+            image: String,
+            lat: Number, 
+            lng: Number
+        }
+    }
+)
+
+const Answer = mongoose.model('Answer', AnswerSchema)
+module.exports = Answer
---
+++ server.js
@@ -0,0 +1,48 @@
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors({
+    origin: process.env.NODE_ENV === 'production'
+    ? process.env.REACT_APP_FRONT_END_PROD
+    : process.env.REACT_APP_FRONT_END_DEV,
+    credentials: true
+}))
+
+//for cookies
+app.use(function(req, res, next) {
+    res.header({
+        "Access-Control-Allow-Origin": process.env.NODE_ENV === 'production'
+            ? process.env.REACT_APP_FRONT_END_PROD
+            : process.env.REACT_APP_FRONT_END_DEV,
+        "Access-Control-Allow-Credentials": "true",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+        "Access-Control-Allow-Headers": "Content-Type, *"
+    })
+    next()
+})
+app.use(cookieParser())
+
+//Controllers 
+const answerControllers = require('./controllers/answerControllers')
+
+
+app.use('/answer', answerControllers)
+app.use('/node_modules', express.static(__dirname + '/node_modules'))
+
+
+app.get('/', (req, res) => {
+    res.send('<h1>We in bois</h1>')
+})
+
+const port = process.env.PORT || 4000
+
+app.listen(port, () => {
+    console.log(`Terra Testr Back End running on port ${port}`)
+})
