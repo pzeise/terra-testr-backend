10353575876142b1f555053cda129ff0adaaa80d
pzeise <phillip.zeise@gmail.com>
2022-07-15 00:37:43 -0400

    did too much: redid seeds, updated models and some routes

--- db/seeding-db.js
+++ db/seeding-db.js
@@ -1,8 +1,15 @@
 const Answer = require('../models/answer-model')
+const User = require('../models/user-model')
 const answerSeeds = require('./answer-seeds.json')
+const userSeeds = require('./user-seeds.json')
 
 
 Answer.deleteMany({})
     .then(() => Answer.create(answerSeeds))
     .then(elements => console.log(`Entered ${elements.length} Answers`))
+    .catch(console.error)
+
+User.deleteMany({})
+    .then(() => User.create(userSeeds))
+    .then(elements => console.log(`Entered ${elements.length} Users`))
     .catch(console.error)
